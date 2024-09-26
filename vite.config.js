8ecd0a7e01606529c68624ac1ce07ef003130f8a
Jeremy Jay <jeremy@pbnjay.com>
2024-09-26 16:43:16 -0400

    adding publishable page

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/evvgas",
   plugins: [react()],
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  }
 })
